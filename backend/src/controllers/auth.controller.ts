fc9b382f134da770f29a3a7c28729c2d313bbb52
bom2 <boo072963@naver.com>
2022-08-16 03:11:56 +0900

    [back] login 시 session 발급, logout 시 session 제거 로직 추가

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -15,19 +15,29 @@ const authController: {
     })
   },
   login: async (req, res, next) => {
-    req.session.userid = 'hi2'
+    const user = await authService.login(req.body)
+
+    // TODO: 이미 세션이 있다면 ?
+    req.session.userid = user.id
+
     res.status(200).json({
-      message: 'login router',
-      sid: req.sessionID,
-      sess: req.session,
+      message: 'success login',
+      user,
     })
   },
   logout: async (req, res, next) => {
-    res.status(200).json({
-      message: 'logout router',
-      sid: req.sessionID,
-      userid: req.session.userid,
-    })
+    if (req.session.userid) {
+      req.session.destroy(console.error)
+
+      res.status(200).json({
+        message: 'success logout',
+        // sid: req.sessionID,
+      })
+    } else {
+      res.status(404).json({
+        message: 'not exist session',
+      })
+    }
   },
 }
 
--- backend/src/services/auth.service.ts
+++ backend/src/services/auth.service.ts
@@ -1,5 +1,6 @@
 import User from '../repositories/user.repository'
 import { CustomError } from '../utils'
+import { stringify as uuidStringify } from 'uuid'
 
 const authService = {
   signup: async (user: {
@@ -23,6 +24,29 @@ const authService = {
 
     const createdUser = await User.create(user)
     return createdUser
+  },
+
+  login: async ({ email, password }: { email: string; password: string }) => {
+    const users = await User.getUserByEmail({
+      email,
+    })
+
+    if (!users.length) {
+      throw new CustomError(404, 'not exist user with matching email')
+    }
+
+    const user = users[0]
+
+    // TODO: password 복호화
+    if (user.password !== password) {
+      throw new CustomError(404, "doesn't not match password")
+    }
+
+    return {
+      id: uuidStringify(user.id),
+      email: user.email,
+      nickname: user.nickname,
+    }
   },
 }
 
