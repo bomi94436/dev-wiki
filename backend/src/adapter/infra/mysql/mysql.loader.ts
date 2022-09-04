c7da06189f0630c581b7e129d72445e550ea3141
bom2 <boo072963@naver.com>
2022-09-05 00:22:07 +0900

    [back] docker compose volumes ingore폴더 추가, database 생성 로직 추가

--- backend/src/adapter/infra/mysql/mysql.loader.ts
+++ backend/src/adapter/infra/mysql/mysql.loader.ts
@@ -1,8 +1,38 @@
 import dataSource from './dataSource'
+import mysql from 'mysql2/promise'
+import config from '../../config'
+
+const pool = mysql.createPool({
+  host: config.mysql.host,
+  port: config.mysql.port,
+  user: config.mysql.user,
+  password: config.mysql.password,
+  timezone: '+09:00',
+  connectionLimit: 10,
+})
+
+const getConnectionPool = async (callback: (conn: mysql.PoolConnection) => any) => {
+  const connection = await pool.getConnection()
+
+  try {
+    callback(connection)
+  } catch (err) {
+    console.error(err)
+  }
+
+  connection.release()
+}
 
 const mysqlLoader = async () => {
-  dataSource
+  // TODO: migration 코드 작성 ?
+  await getConnectionPool(async (conn) => {
+    await conn.query('CREATE DATABASE IF NOT EXISTS dev_wiki_db default CHARACTER SET UTF8')
+    await conn.query('USE dev_wiki_db')
+  })
+
+  await dataSource
     .initialize()
+    .then(async () => {})
     .then(() => {
       console.log('typeorm app data source is ready')
     })
