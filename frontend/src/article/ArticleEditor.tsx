7c988f34a8a2a920f2bfb3f470a2ee8cf9435921
bom2 <boo072963@naver.com>
2022-10-02 22:50:26 +0900

    [front] article 작성 시 drop 및 paste로 이미지 업로드 기능 추가

--- frontend/src/article/ArticleEditor.tsx
+++ frontend/src/article/ArticleEditor.tsx
@@ -1,11 +1,60 @@
-import API from '@/global/api'
+import React, { useState } from 'react'
 import { Button } from '@mui/material'
 import MDEditor from '@uiw/react-md-editor'
-import React, { useState } from 'react'
+import API from '@/global/api'
+
+const insertToTextArea = (intsertString: string) => {
+  const textarea = document.querySelector('textarea')
+  if (!textarea) {
+    return null
+  }
+
+  let sentence = textarea.value
+  const len = sentence.length
+  const pos = textarea.selectionStart
+  const end = textarea.selectionEnd
+
+  const front = sentence.slice(0, pos)
+  const back = sentence.slice(pos, len)
+
+  sentence = front + intsertString + back
+
+  textarea.value = sentence
+  textarea.selectionEnd = end + intsertString.length
+
+  return sentence
+}
+
+const onImagePasted = async (
+  dataTransfer: DataTransfer,
+  setMarkdown: React.Dispatch<React.SetStateAction<string>>
+) => {
+  const files: File[] = []
+
+  for (let index = 0; index < dataTransfer.items.length; index += 1) {
+    const file = dataTransfer.files.item(index)
+
+    if (file) {
+      files.push(file)
+    }
+  }
+
+  await Promise.all(
+    files.map(async (file) => {
+      // TODO: image upload
+      const url = await API.post('/upload', file).catch(() => 'test_url')
+      const insertedMarkdown = insertToTextArea(`![](${url})`)
+      if (!insertedMarkdown) {
+        return
+      }
+      setMarkdown(insertedMarkdown)
+    })
+  )
+}
 
 const ArticleEditor: React.FC = () => {
   const [title, setTitle] = useState<string>('')
-  const [content, setContent] = useState<string>('**내용을 작성하세요**')
+  const [content, setContent] = useState<string>('')
 
   const onSubmitCreateArticle: React.FormEventHandler = (e) => {
     e.preventDefault()
@@ -25,7 +74,22 @@ const ArticleEditor: React.FC = () => {
         />
       </div>
 
-      <MDEditor value={content} onChange={setContent} height={400} />
+      <MDEditor
+        value={content}
+        onChange={setContent}
+        height={400}
+        onPaste={async (event) => {
+          event.preventDefault()
+          await onImagePasted(event.clipboardData, setContent)
+        }}
+        onDrop={async (event) => {
+          event.preventDefault()
+          await onImagePasted(event.dataTransfer, setContent)
+        }}
+        textareaProps={{
+          placeholder: '내용을 작성하세요',
+        }}
+      />
 
       <div className="pt-6 flex justify-end">
         <Button variant="contained" type="submit">
