7b4b73fde71dfea7dda7e36ec0502c86075e8b4b
bom2 <boo072963@naver.com>
2022-10-31 23:00:31 +0900

    [back] task-card 하위 task 조회하는 query builder 작성

--- backend/src/repository/task.repository.impl.ts
+++ backend/src/repository/task.repository.impl.ts
@@ -11,14 +11,20 @@ class TaskRepositoryImpl implements TaskRepository {
   }
 
   public async getList(taskCardId: number): Promise<Task[]> {
-    return await this.taskRepository.find({
-      where: {
-        task_card_id: taskCardId,
-      },
-      relations: {
-        tasks: true,
-      },
-    })
+    return await this.taskRepository
+      .createQueryBuilder()
+      .select(['t1'])
+      .from(Task, 't1')
+      .leftJoinAndMapMany('t1.tasks', 'task', 't2', 't1.task_id = t2.parent_task_id')
+      .leftJoinAndMapMany('t2.tasks', 'task', 't3', 't2.task_id = t3.parent_task_id')
+      .where('t1.parent_task_id IS NULL')
+      .andWhere('t1.task_card_id = :taskCardId', { taskCardId })
+      .orderBy({
+        't1.created_at': 'DESC',
+        't2.created_at': 'DESC',
+        't3.created_at': 'DESC',
+      })
+      .getMany()
   }
 }
 
