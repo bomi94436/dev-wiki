a25e0a6009d45ad1e106e730e965f28877659fa3
bom2 <boo072963@naver.com>
2023-05-11 00:05:46 +0900

    [back] cors 동작방식 변경

--- backend/src/infra/express/express.loader.ts
+++ backend/src/infra/express/express.loader.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import session from 'express-session'
 import connectRedis from 'connect-redis'
 import { v4 as uuidv4 } from 'uuid'
-import cors, { CorsOptions } from 'cors'
+import cors from 'cors'
 import path from 'path'
 
 import { SESSION_KEY, STATIC_UPLOAD_FOLDER_PATH } from 'global/constant'
@@ -22,16 +22,6 @@ import {
 const RedisStore = connectRedis(session)
 
 const whitelist = ['http://localhost:3000', 'http://localhost:5001']
-const corsOptions: CorsOptions = {
-  origin: function (origin, callback) {
-    if (origin && whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  },
-  credentials: true,
-}
 
 const expressLoader = async ({ app }: { app: express.Express }) => {
   app.use(express.json())
@@ -44,7 +34,7 @@ const expressLoader = async ({ app }: { app: express.Express }) => {
     STATIC_UPLOAD_FOLDER_PATH,
     express.static(path.join(__dirname, getRelativePathOfProjectRootPath(__dirname), 'uploads'))
   )
-  app.use(cors(corsOptions))
+  app.use(cors({ origin: whitelist, credentials: true }))
 
   /**
    * redis를 store로 한 session 설정
