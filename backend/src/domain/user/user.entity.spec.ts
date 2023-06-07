fcae9aff7630f0386c62a76742cd6743497a64e8
bom2 <boo072963@naver.com>
2023-06-08 00:04:03 +0900

    [back] 테스트 라이브러리 설치, 실행명령어 추가, mocha 테스트용 파일 작성

---
+++ backend/src/domain/user/user.entity.spec.ts
@@ -0,0 +1,9 @@
+import assert from 'assert'
+
+describe('User Example', function () {
+  describe('calculation', function () {
+    it('1+1 should be 2', function () {
+      assert.equal(1 + 1, 2)
+    })
+  })
+})
